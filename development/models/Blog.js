8a5298232643ab020da9b708cb02f6eab6eac9de
Linovo <asif88383@gmail.com>
2022-03-25 18:49:06 +0500

    created shcema for blogDB

---
+++ development/models/Blog.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import Bluebird from "bluebird";
+
+const { Schema } = mongoose;
+
+mongoose.Promise = Bluebird;
+
+const BlogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+const Blog = mongoose.model("Blog", BlogSchema);
+
+export default Blog;
