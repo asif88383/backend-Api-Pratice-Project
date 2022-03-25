8e41740b5b768908a1fcdb5761ab14a51d063a08
Linovo <asif88383@gmail.com>
2022-03-25 15:17:04 +0500

    fetch data from api for bitcoin

--- integration/app.js
+++ integration/app.js
@@ -19,6 +19,25 @@ const ethData = async()=>{
     const response = await fetch(apiKey);
     const json = await response.json();
     const data = json.Data.Data;
+    const times = data.map(key => key.high);
+    const prices = data.map(key => key.high);
+    return{
+        times,
+        prices
+    }
+
+}
 
 
+let BTCChart;
+
+async function createBTCChart(){
+    let {times,prices} = await btcData();
+    let btcChart = document.getElementById('btcChart').getContext('2d');
+    let gradient = btcChart.createLinearGradient(0,0,0,400);
+
+    gradient.addColorStop(0, 'rgba(247, 147, .5)');
+    BTCChart = new Chart(btcChart, {
+        
+    });
 }
