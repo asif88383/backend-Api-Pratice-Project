55b5b2fab3ee24dbad092b4286e5b794ed1a1e08
Linovo <asif88383@gmail.com>
2022-03-25 19:22:06 +0500

    add mongodb database connection

--- development/app.js
+++ development/app.js
@@ -1,3 +1,19 @@
 import express from 'express'; // ES6 notation
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import routes from './routes.js';
+import blogController from './controllers/BlogController.js';
+
+
+let uri = "";
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true }, () =>{
+    console.log("Connected to mongodb");
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use.apply('/api', routes);
+
+
+export default app;
