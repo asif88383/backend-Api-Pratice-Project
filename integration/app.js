eef3b9028056824e733a18c1babacc3b94535809
Linovo <asif88383@gmail.com>
2022-03-25 15:26:06 +0500

    fetch data from api for etherium

--- integration/app.js
+++ integration/app.js
@@ -38,6 +38,50 @@ async function createBTCChart(){
 
     gradient.addColorStop(0, 'rgba(247, 147, .5)');
     BTCChart = new Chart(btcChart, {
-        
+        type: 'line',
+        data:{
+            lables: times,
+            datasets:[{
+                lable: '$',
+                data:prices,
+                backgroundColor:gradient,
+                borderColor:'#f7931e',
+                borderWidth: 3,
+                borderJointStyle: 'round',
+                pointHitRadius: 10,
+                borderCapStyle: 'round',
+                pointRadius:0,
+                lineTension: .2,
+            }]
+        },
+        options: {
+            legend:{
+                display: false
+            },
+            title: {
+                display: false,
+                text: 'Bitcoin Price'
+            },
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0
+                }
+            },
+            scales: {
+                yAxis: [{
+                    display: false,
+                    gridLines: {}
+                }],
+                xAxis: [{
+                    display: false,
+                    gridLines: {}
+                }]
+            },
+        }
     });
 }
+
+
