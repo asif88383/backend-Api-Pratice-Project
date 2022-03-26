6b838936c6ed38d393c7c6d143538e638eac460f
Linovo <asif88383@gmail.com>
2022-03-26 16:34:28 +0500

    added mongodb database link

--- development/app.js
+++ development/app.js
@@ -49,8 +49,3 @@ console.log("server updated")
 console.log("server updated")
 console.log("server updated")
 
-console.log("server updated")
-console.log("server updated")
-console.log("server updated")
-
-
